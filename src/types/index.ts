57bdb0c0b5ae18f1f7fc7a3b92d91b748ebf1fa7
camachoo1 <ocamacho812@gmail.com>
2025-05-23 02:46:56 -0700

    Initial commit

---
+++ src/types/index.ts
@@ -0,0 +1,45 @@
+export interface Player {
+  id: number;
+  name: string;
+  locked: boolean;
+  teamId: number | null;
+}
+
+export interface Team {
+  id: number;
+  name: string;
+  players: Player[];
+}
+
+export interface HistoryEntry {
+  id: number;
+  timestamp: string;
+  teams: Team[];
+  eventName: string;
+  organizerName: string;
+}
+
+export interface Store {
+  // State
+  eventName: string;
+  organizerName: string;
+  players: Player[];
+  teams: Team[];
+  teamSize: number;
+  history: HistoryEntry[];
+  darkMode: boolean;
+
+  // Actions
+  setEventInfo: (eventName: string, organizerName: string) => void;
+  addPlayer: (playerName: string) => void;
+  removePlayer: (playerId: number) => void;
+  togglePlayerLock: (playerId: number) => void;
+  setTeamSize: (size: number) => void;
+  randomizeTeams: () => void;
+  updateTeamName: (teamId: number, newName: string) => void;
+  movePlayerToTeam: (playerId: number, newTeamIndex: number) => void;
+  loadHistoryEntry: (historyId: number) => void;
+  clearAll: () => void;
+  clearHistory: () => void;
+  toggleDarkMode: () => void;
+}
